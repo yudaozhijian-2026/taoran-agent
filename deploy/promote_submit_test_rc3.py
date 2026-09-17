f35a96332b8d295b7c5d5faf030914686bf2c12b
yudaozhijian-2026 <yudaozhijian2026@gmail.com>
2026-09-17 11:33:37 +0800

    handle dated isolated candidate promotion

--- deploy/promote_submit_test_rc3.py
+++ deploy/promote_submit_test_rc3.py
@@ -21,7 +21,8 @@ previous = {
     '1.0.6rc11': '1.0.6rc10',
     '1.0.6rc12': '1.0.6rc11',
 }[release]
-old = f'taoran-submit-test:{previous}-20260916'
+old_date_suffix = '20260917' if previous in {'1.0.6rc11', '1.0.6rc12'} else '20260916'
+old = f'taoran-submit-test:{previous}-{old_date_suffix}'
 date_suffix = '20260917' if release in {'1.0.6rc11', '1.0.6rc12'} else '20260916'
 new = f'taoran-submit-test:{release}-{date_suffix}'
 info = json.loads(subprocess.check_output(['docker', 'inspect', 'taoran-submit-test-agent']))[0]
